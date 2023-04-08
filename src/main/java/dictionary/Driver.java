efe4b37ecf020f16b1b22ed3cad2f54abd4d4b9e
nitzansaar <112202258+nitzansaar@users.noreply.github.com>
2023-04-08 14:01:43 -0700

    Added tests to driver

--- src/main/java/dictionary/Driver.java
+++ src/main/java/dictionary/Driver.java
@@ -14,35 +14,22 @@ public class Driver {
             dict.add("wrath");
             dict.add("wristle");
             System.out.println(dict);
-            testAdd();
+            System.out.println("----------------");
             testGetCommonPrefix();
             testToString();
             testCheck();
             testCheckPrefix();
     }
         private static CompactPrefixTree compactPrefixTree;
-        private static void testAdd() {
-                CompactPrefixTree tree = new CompactPrefixTree();
-                tree.add("ham");
-                System.out.println("Tree: ");
-                System.out.println(tree);
-                tree.add("hamster");
-                System.out.println("Tree: ");
-                System.out.println(tree);
-                tree.add("hamburger");
-                System.out.println("Tree: ");
-                System.out.println(tree);
-                System.out.println("----------------");
-        }
+
         private static void testGetCommonPrefix() {
-                System.out.println();
                 String s1 = "catastrophe";
                 String s2 = "cater";
                 int i = 0;
                 while (s1.charAt(i) == s2.charAt(i)) {
                         i++;
                 }
-                System.out.println(s1.substring(0, i));
+                System.out.println("cat: " + s1.substring(0, i));
                 System.out.println("----------------");
         }
 
